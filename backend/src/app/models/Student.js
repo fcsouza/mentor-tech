f9e46ada08955926c8322170043ab9e53694f966
fcsouza <fcsouza18@hotmail.com>
2020-09-07 21:31:30 -0300

    include student model

---
+++ backend/src/app/models/Student.js
@@ -0,0 +1,25 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Student extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        tel: Sequelize.STRING,
+        active: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.School, { foreignKey: 'school_id' });
+  }
+}
+
+export default Student;
