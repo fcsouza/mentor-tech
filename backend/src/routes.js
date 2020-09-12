8f1c117fd8cdfd0982cf0a87cfed9007c41a1eb1
fcsouza <fcsouza18@hotmail.com>
2020-09-11 22:55:15 -0300

    include routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,1 +1,7 @@
 import { Router } from 'express';
+
+import MentorController from './app/controllers/MentorController';
+import SchoolController from './app/controllers/SchoolController';
+import WorkshopController from './app/controllers/WorkshopController';
+import WorkshopAllController from './app/controllers/WorkshopAllController';
+import StudentController from './app/controllers/StudentController';
