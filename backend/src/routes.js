2048f96bc50a96216d903ce496d621b46a13bf41
fcsouza <fcsouza18@hotmail.com>
2020-09-11 22:55:39 -0300

    include mentors

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,3 +5,8 @@ import SchoolController from './app/controllers/SchoolController';
 import WorkshopController from './app/controllers/WorkshopController';
 import WorkshopAllController from './app/controllers/WorkshopAllController';
 import StudentController from './app/controllers/StudentController';
+
+const routes = new Router();
+
+routes.get('/mentors', MentorController.index);
+routes.post('/mentors', MentorController.store);
