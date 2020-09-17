8a888d41190e2bc12b77668f16691dd2c25a2737
fcsouza <fcsouza18@hotmail.com>
2020-09-16 22:03:01 -0300

    include default layout

---
+++ frontend/src/layouts/default/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  min-height: 100%;
+  background: #fff;
+`;
+
+export const Container = styled.div`
+  padding: 0 30px;
+  overflow: auto;
+`;
+
+export const Content = styled.div`
+  max-width: 940px;
+  margin: 40px auto;
+`;
